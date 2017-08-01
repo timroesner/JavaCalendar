9503c852c8ff935c5b40618d0830930f3ce19fa8
Tim Roesner <timroesner@me.com>
2017-07-31 19:45:32 -0700

    WeekView, Today Btn & View Btns

--- calender/src/calendar/DayViewPanel.java
+++ calender/src/calendar/DayViewPanel.java
@@ -14,28 +14,28 @@ import javax.swing.JScrollPane;
 import javax.swing.SwingConstants;
 
 public class DayViewPanel extends JPanel {
-	
+
 	private JLabel[] labels = new JLabel[24];
 	private ArrayList<Event> eventsOnDay = new ArrayList<>();
-	
+
 	public DayViewPanel() {
-		
-        // Enclosing panel with GridBagLayout
+
+		// Enclosing panel with GridBagLayout
 		JPanel panel = new JPanel();
 		panel.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		c.fill = GridBagConstraints.HORIZONTAL;
-		
-        // Load all events happening that day
-		for(Event event : Calendar.events){
-			if(event.date.equals(SimpleCalendar.month.date)){
+
+		// Load all events happening that day
+		for (Event event : Calendar.events) {
+			if (event.date.equals(SimpleCalendar.month.date)) {
 				eventsOnDay.add(event);
 				System.out.println(event.title);
 			}
 		}
-		
-        // Create all 24 lables for each hour of the day
-		for(int hour = 0; hour<24;hour++){
+
+		// Create all 24 lables for each hour of the day
+		for (int hour = 0; hour < 24; hour++) {
 			LocalTime time = LocalTime.of(hour, 0);
 			JLabel timeLbl = new JLabel(time.toString());
 			timeLbl.setHorizontalAlignment(SwingConstants.CENTER);
@@ -46,19 +46,19 @@ public class DayViewPanel extends JPanel {
 			gap.setBackground(Color.white);
 			gap.setOpaque(true);
 			gap.setBorder(BorderFactory.createLineBorder(Color.black));
-            
-            // If event is hapning make background blue and add title of events
-			for(Event event : eventsOnDay){
-				if(event.startTime.getHour() == hour){
+
+			// If event is hapning make background blue and add title of events
+			for (Event event : eventsOnDay) {
+				if (event.startTime.getHour() == hour) {
 					labels[hour].setText(event.title);
 					labels[hour].setBackground(Color.BLUE);
 				}
-				if(event.startTime.getHour() < hour && hour < event.endTime.getHour()){
+				if (event.startTime.getHour() < hour && hour < event.endTime.getHour()) {
 					labels[hour].setBackground(Color.BLUE);
 				}
 			}
-            
-            // Add labels with preffered size
+
+			// Add labels with preffered size
 			labels[hour] = gap;
 			c.gridx = 0;
 			c.gridy = hour;
@@ -71,35 +71,35 @@ public class DayViewPanel extends JPanel {
 			gap.setPreferredSize(new Dimension(250, 25));
 			panel.add(gap, c);
 		}
-		
-        // Make panel scrollable
+
+		// Make panel scrollable
 		JScrollPane scrollPane = new JScrollPane(panel);
 		scrollPane.setPreferredSize(new Dimension(325, 200));
 		add(scrollPane);
 	}
-	
-    // Called when month or event model is updated
-	public void update(Month month){
+
+	// Called when month or event model is updated
+	public void update(Month month) {
 		eventsOnDay.clear();
-        
-        // Update events happening that day
-		for(Event event : Calendar.events){
-			if(event.date.equals(month.date)){
+
+		// Update events happening that day
+		for (Event event : Calendar.events) {
+			if (event.date.equals(month.date)) {
 				eventsOnDay.add(event);
 				System.out.println(event.title);
 			}
 		}
-        
-        // Update labels and add new events
-		for(int hour=0;hour<24;hour++){
+
+		// Update labels and add new events
+		for (int hour = 0; hour < 24; hour++) {
 			labels[hour].setText("");
 			labels[hour].setBackground(Color.WHITE);
-			for(Event event : eventsOnDay){
-				if(event.startTime.getHour() == hour){
+			for (Event event : eventsOnDay) {
+				if (event.startTime.getHour() == hour) {
 					labels[hour].setText(event.title);
 					labels[hour].setForeground(Color.white);
 					labels[hour].setBackground(Color.BLUE);
-				} else if(event.startTime.getHour() < hour && hour < event.endTime.getHour()){
+				} else if (event.startTime.getHour() < hour && hour < event.endTime.getHour()) {
 					labels[hour].setBackground(Color.BLUE);
 				}
 			}
--- calender/src/calendar/Month.java
+++ calender/src/calendar/Month.java
@@ -15,51 +15,68 @@ import java.text.DateFormatSymbols;
  */
 
 public class Month {
-    
-    public LocalDate date;
-    private ArrayList<ChangeListener> listeners;
-    
-    public Month(LocalDate date){
-        this.date = date;
-        listeners = new ArrayList<ChangeListener>();
-    }
-    
-    /**
-     * Attach a listener to the Model
-     * @param c the listener
-     */
-    public void attach(ChangeListener c){
-        listeners.add(c);
-    }
-    
-    // Changes current date and notifies listeners
-    public void changeDate(LocalDate date){
-        this.date = date;
-        for (ChangeListener l : listeners) {
-            l.stateChanged(new ChangeEvent(this));
-        }
-    }
-    
-    // Returns array with all 42 days to be displayed
-    public LocalDate[] getDateArray(){
-        LocalDate firstOfMonth = this.date.withDayOfMonth(1);
-        LocalDate currentDay = firstOfMonth.minusDays(firstOfMonth.getDayOfWeek().getValue());
-        LocalDate[] result = new LocalDate[42];
-        for(int day = 0; day<42; day++){
-            result[day] = currentDay;
-            currentDay = currentDay.plusDays(1);
-        }
-        return result;
-    }
-    
-    // Returns localized strings of weekdays
-    public String[] weekdays(){
-        DateFormatSymbols symbols = new DateFormatSymbols(SimpleCalendar.language);
-        return symbols.getShortWeekdays();
-    }
-    
-    // Returns localized string of month and year
-    public String monthYearString(){
-        return this.date.getMonth().getDisplayName( TextStyle.FULL , SimpleCalendar.language )+ " "+this.date.getYear();
-    }
+
+	public LocalDate date;
+	private ArrayList<ChangeListener> listeners;
+
+	public Month(LocalDate date) {
+		this.date = date;
+		listeners = new ArrayList<ChangeListener>();
+	}
+
+	/**
+	 * Attach a listener to the Model
+	 * 
+	 * @param c
+	 *            the listener
+	 */
+	public void attach(ChangeListener c) {
+		listeners.add(c);
+	}
+
+	// Changes current date and notifies listeners
+	public void changeDate(LocalDate date) {
+		this.date = date;
+		for (ChangeListener l : listeners) {
+			l.stateChanged(new ChangeEvent(this));
+		}
+	}
+
+	// Returns array with all 42 days to be displayed
+	public LocalDate[] getDateArray() {
+		LocalDate firstOfMonth = this.date.withDayOfMonth(1);
+		LocalDate currentDay = firstOfMonth.minusDays(firstOfMonth.getDayOfWeek().getValue());
+		LocalDate[] result = new LocalDate[42];
+		for (int day = 0; day < 42; day++) {
+			result[day] = currentDay;
+			currentDay = currentDay.plusDays(1);
+		}
+		return result;
+	}
+
+	public LocalDate[] getWeekArray() {
+		LocalDate currentDate = this.date;
+		LocalDate[] result = new LocalDate[7];
+		for (int day = 0; day < 7; day++) {
+			if (day == 0) {
+				if (currentDate.getDayOfWeek().getValue() != 7) {
+					currentDate = currentDate.minusDays(currentDate.getDayOfWeek().getValue());
+				}
+			}
+			result[day] = currentDate;
+			currentDate = currentDate.plusDays(1);
+		}
+		return result;
+	}
+
+	// Returns localized strings of weekdays
+	public String[] weekdays() {
+		DateFormatSymbols symbols = new DateFormatSymbols(SimpleCalendar.language);
+		return symbols.getShortWeekdays();
+	}
+
+	// Returns localized string of month and year
+	public String monthYearString() {
+		return this.date.getMonth().getDisplayName(TextStyle.FULL, SimpleCalendar.language) + " " + this.date.getYear();
+	}
 }
